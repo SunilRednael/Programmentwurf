0ef78ba0f0240111fa3f6af3080ec8fdf941f73f
mathisstuetzle <122612123+mathisstuetzle@users.noreply.github.com>
2023-03-15 20:54:39 +0000

    .

--- include/developer.h
+++ include/developer.h
@@ -1,18 +1,19 @@
 #ifndef DEVELOPER_H_
 #define DEVELOPER_H_
 
-#define name(5U)
-#define alias(25U)
+#define name
+#define alias
 
 typedef struct
 {
     unsigned int id;
-    char name[name];
-    char alias[alias];
+    char name(NAME);
+    char alias(ALIAS);
 } developer;
 
-void developer_init(developer *const s, const char name[]);
+void developer_init(developer *const s, const char name()); 
 void developer_print(developer const *const s);
-void developer_add_grade(developer *const s, char alias[]);
+void developer_add_alias(developer *const s, char alias());       
 
-#endif /* DEVELOPER_H_ */
+
+#endif 

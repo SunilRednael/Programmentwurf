e3e8959050c55bbd2b8f2e62c8c22541758426ef
SunilRednael <122459030+SunilRednael@users.noreply.github.com>
2023-03-20 20:21:57 +0000

    cmake changes2

---
+++ build/CMakeFiles/exec_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exec_main.dir/src/main.c.o"
+  "exec_main"
+  "exec_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exec_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lib1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib1.dir/src/developer.c.o"
+  "CMakeFiles/lib1.dir/src/developer_group.c.o"
+  "liblib1.a"
+  "liblib1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
